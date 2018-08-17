a74cb0781efb48f5a534123fa9adbdd17fe3bcba
Rohit Goswami <lynus.mascot@gmail.com>
2018-08-17 13:16:26 +0530

    Create Swap without third variable.c

---
+++ Swap without third variable.c
@@ -0,0 +1,27 @@
+//Author : Rohit Goswami
+// Code to swap two numbers using third varibale
+#include<stdio.h>
+#include<conio.h>
+
+int main(int argc, char *argv[]){
+    
+    if(argc>=2){
+        
+        int a,b;
+        a=atoi(argv[1]);
+        b=atoi(argv[2]);
+ 
+        printf("a : %d and b : %d before swap\n",a,b);
+        
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        
+        printf("a : %d and b : %d after swap\n",a,b);
+        
+    }
+    
+    else{
+        printf("too few arguments\n");
+    }
+}
