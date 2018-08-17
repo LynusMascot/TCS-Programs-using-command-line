a2ec869e7e759e07d9deb104ec30be99c618647b
Rohit Goswami <lynus.mascot@gmail.com>
2018-08-17 14:43:42 +0530

    Create Palindrome number.c

---
+++ Palindrome number.c
@@ -0,0 +1,44 @@
+//Author : Rohit Goswami
+// Code to check palindrome number
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+int main(int argc, char *argv[]){
+    
+    if(argc>=2){
+        
+        int n,i;
+        
+        int c=0;
+        n=atoi(argv[1]);
+        int orig=n;
+        int temp=n;
+        
+       while(temp!=0){
+           temp=temp/10;
+           c++;
+       }
+
+       int num=0;
+       for(i=c;i>0;i--){
+           
+           num=(num+n%10)*10;
+           n=n/10;
+         
+       }
+       num=num/10;
+       
+       printf("num : %d\n",num);
+       printf("n: %d\n",orig);
+       
+       
+       if(num==orig){ printf("Number is Palindrome");}
+       else{ printf("Number is not Palindrome");}
+        
+    }
+    
+    else{
+        printf("too few arguments\n");
+    }
+}
